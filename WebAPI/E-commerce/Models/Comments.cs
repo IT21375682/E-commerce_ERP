92f92ec59dea4964662ea013dd7d7d2d7e7f6192
Shandeep <shandeepsanju@gmail.com>
2024-09-28 14:23:39 +0530

    Shandeep's First commit

---
+++ WebAPI/E-commerce/Models/Comments.cs
@@ -0,0 +1,12 @@
+﻿namespace E_commerce.Models
+{
+    public class Comment
+    {
+        public string CommentId { get; set; }  // Unique identifier for the comment
+        public string Id { get; set; }  // ID of the user who made the comment
+        public string ProductId { get; set; }  // ID of the product the comment is associated with
+        public DateTime Date { get; set; }  // Date the comment was made
+        public int Rating { get; set; }  // Rating given to the product (e.g., 1-5 stars)
+        public string CommentText { get; set; }  // The actual comment text
+    }
+}
---
+++ WebAPI/E-commerce/Models/Login.cs
@@ -0,0 +1,9 @@
+﻿namespace E_commerce.Models
+{
+    public class Login
+    {
+        public string Email { get; set; }   // Email for login and contact
+        public string Password { get; set; }   // User password (make sure to store it securely, hashed)
+
+    }
+}
